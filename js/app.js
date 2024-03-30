7b33e66eec43093abedbaa89ea4cd608eabb219e
amirafarag <amiraazam301@gmail.com>
2024-03-30 06:00:49 +0200

    new

--- js/app.js
+++ js/app.js
@@ -7,34 +7,27 @@ console.log(arr);
 
 var index = 0;
 while (index < arr.length)  {
-    if (arr[index] = 4) {
-        console.log(`before adding 00: number = ${arr[index]} after adding 00: number = ${arr[index] = "004"}`);
-    }
-
-    if (arr[index] = 9) {
-        console.log(`before adding 00: number = ${arr[index]} after adding 00: number = ${arr[index] = "009"}`);
+    if (arr[index] <= 9) {
+        console.log(`before adding 00: number = ${arr[index]} after adding 00: number = 00${arr[index]}`);
+        console.log(arr[index] , arr[index] = '0'+ arr[index] );
     }
 
     // 2-add 1 leading zero 0 and concatenate it with each number that is less than or equal 99
-    
-    if (arr[index] = 10) {
-        console.log(`before adding 0: number = ${arr[index]} after adding 0: number = ${arr[index] ="010"}`);
+    else if (arr[index] <= 99) {
+        console.log(`before adding 0: number = ${arr[index]} after adding 0: number = 0${arr[index]}`);
+        console.log(arr[index] , arr[index] = '00'+ arr[index] );
     }
 
-    if (arr[index] = 28) {
-        console.log(`before adding 0: number = ${arr[index]} after adding 0: number = ${arr[index] ="028"}`);
+    // 3-225 is greater than 99
+    if (arr[index] > 99) {
+        console.log(`225 is greater than 99`);
+        console.log(arr[index]);
     }
-
-    if (arr[index] = 47) {
-        console.log(`before adding 0: number = ${arr[index]} after adding 0: number = ${arr[index] ="047"}`);
-    }
-
 
     index += 1;
 }
 
-// var upDate = [010, 200, 004, 028, 047, 009, 225];
-// console.log(upDate);
+
 
 // 1-what is wrong
 // 2-what is the easy way
